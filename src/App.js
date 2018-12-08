48b121252cc51e0849040a69d7d6328c32501f5b
varun sharma <varunsharma12@outlook.com>
2018-12-08 21:08:50 +0530

    A React App talking to spring boot backend Api

--- src/App.js
+++ src/App.js
@@ -3,26 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  state = {
+    groups: [],
+    isLoading: true,
+  };
+
+  async componentDidMount(){
+      const response = await fetch("/api/groups");
+      const body = await response.json();
+      this.setState({
+        groups: body,
+        isLoading: false
+      });
   }
+
+  render(){
+    const {groups, isLoading} = this.state;
+    
+    if(isLoading){
+      return <p>Loading...</p>
+    }
+return (
+    <div className="App">
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <div className="App-intro">
+        <h2>Jug Tours</h2>
+        {groups.map(group => <div key={group.id}>
+        {group.name}
+        </div>)}
+      </div>
+      </header>
+    </div>
+);
+  }
+
 }
 
 export default App;
